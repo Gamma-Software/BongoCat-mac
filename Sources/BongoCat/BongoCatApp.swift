4d2639cce99579f74ad05132ff423ffc71ccb5fc
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2025-08-07 15:30:22 +0200

    bump version to v1.8.2

--- Sources/BongoCat/BongoCatApp.swift
+++ Sources/BongoCat/BongoCatApp.swift
@@ -24,7 +24,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate, ObservableOb
 
     // App information
     private let appVersion = "1.8.2"
-    private let appBuild = "1.8.2.202508071329"
+    private let appBuild = "1.8.2.202508071330"
     private let appAuthor = "Valentin Rudloff"
     private let appWebsite = "https://valentin.pival.fr"
 
